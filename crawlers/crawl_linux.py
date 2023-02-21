d6ff49de2c951db5647257353751f29968ea5b6f
donaghy_mo <504643635@qq.com>
2023-02-21 10:53:14 +0800

    修改小木虫专业筛除

--- crawlers/crawl_linux.py
+++ crawlers/crawl_linux.py
@@ -9,6 +9,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 import os
 import sys
+
 sys.path.append(os.path.abspath(__file__).split("crawlers")[0])
 from channel import wechat
 from common.log import logger
@@ -16,7 +17,7 @@ from common.log import logger
 time_match = re.compile("\d{4}-\d{2}-\d{2}")
 useless_key_words = ["无查询结果"]
 engineering = ["工学"]
-expertises = ["网络空间安全","计算机","电子信息"]
+expertises = ["网络空间安全", "计算机", "电子", "网络", "计算机科学与技术","电子信息"]
 # 爬虫开始的日期
 begin_date = datetime.date.today()
 # begin_date = datetime.date(2022,2,15)
@@ -29,11 +30,19 @@ def parse_time(time_str):
     year, month, day = int(year), int(month), int(day)
     return datetime.date(year, month, day)
 
+
 def is_msg_exist(msg):
-    fp = open(file_name,"r")
+    fp = open(file_name, "r")
     if msg in fp.read():
         # TODO:把消息发出去
         return True
+    return False
+
+
+def is_computer_science(msg):
+    for i in expertises:
+        if i in msg:
+            return True
     return False
 
 
@@ -43,7 +52,10 @@ def crawl_msg():
         "fangban": "http://wyy.gzhu.edu.cn/zsjy/yjszs.htm",
         "guangzhoudaxue": "http://yjsy.gzhu.edu.cn/zsxx/zsdt.htm",
         "fujianshifan": "https://yjsy.fjnu.edu.cn/4227/list1.htm",
-        "xiaomuchong": ["http://muchong.com/bbs/kaoyan.php?action=adjust&type=1","http://muchong.com/bbs/kaoyan.php?action=adjust&type=1&page=1&page=2","http://muchong.com/bbs/kaoyan.php?action=adjust&type=1&page=1&page=3","http://muchong.com/bbs/kaoyan.php?action=adjust&type=1&page=3&page=4"]
+        "xiaomuchong": ["http://muchong.com/bbs/kaoyan.php?action=adjust&type=1",
+                        "http://muchong.com/bbs/kaoyan.php?action=adjust&type=1&page=1&page=2",
+                        "http://muchong.com/bbs/kaoyan.php?action=adjust&type=1&page=1&page=3",
+                        "http://muchong.com/bbs/kaoyan.php?action=adjust&type=1&page=3&page=4"]
     }
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
@@ -56,72 +68,72 @@ def crawl_msg():
     params = {"xm": "莫登意", "zjhm": "430105199806035615", "ksbh": "100133412881861", "bkdwdm": "10013"}
     req = requests.post(url_dic["yanzhao"], params, headers=headers)
     if "无查询结果" in req.text:
-        wchannel.send("【研招网】成绩还查不到\n",get_mo_id())
+        wchannel.send("【研招网】成绩还查不到\n", get_mo_id())
     else:
-        wchannel.send("【研招网】成绩查到了！！\n",get_mo_id())
-
+        wchannel.send("【研招网】成绩查到了！！\n", get_mo_id())
 
     # 广州大学方班最新消息
     req = requests.get(url_dic["fangban"], headers=headers)
     req.encoding = "utf-8"
-    fangban_b = bs4.BeautifulSoup(req.text,"html.parser")
+    fangban_b = bs4.BeautifulSoup(req.text, "html.parser")
     for i in fangban_b.select("a > span"):
-        mesg_date_str = re.findall(time_match,str(i.find_parent()))
+        mesg_date_str = re.findall(time_match, str(i.find_parent()))
         if mesg_date_str:
-            year,month,day = mesg_date_str[0].split('-')
-            year,month,day = int(year),int(month),int(day)
-            mesg_date = datetime.date(year,month,day)
+            year, month, day = mesg_date_str[0].split('-')
+            year, month, day = int(year), int(month), int(day)
+            mesg_date = datetime.date(year, month, day)
             if mesg_date > begin_date:
-                mesg = "【广州大学方班{}】有新消息{}\n".format(mesg_date ,re.sub(r"<.*?>|&nbsp;|\n","",str(i.find_parent())))
+                mesg = "【广州大学方班{}】有新消息{}\n".format(mesg_date,
+                                                             re.sub(r"<.*?>|&nbsp;|\n", "", str(i.find_parent())))
                 if not is_msg_exist(mesg):
                     flag = False
-                    msg_to_write = msg_to_write+mesg
+                    msg_to_write = msg_to_write + mesg
 
     # 广州大学研究生院
     req = requests.get(url_dic["guangzhoudaxue"], headers=headers)
     req.encoding = "utf-8"
     guangda_b = bs4.BeautifulSoup(req.text, "html.parser")
     for i in guangda_b.find_all('a'):
-        mesg_time = re.findall(time_match,str(i))
+        mesg_time = re.findall(time_match, str(i))
         if mesg_time:
             if parse_time(mesg_time[0]) > begin_date:
 
-                i = re.findall(r"title=(.+?)\"",str(i))
-                mesg = "【广州大学研究生院{}】有新消息：{}\n".format(parse_time(mesg_time[0]),i)
+                i = re.findall(r"title=(.+?)\"", str(i))
+                mesg = "【广州大学研究生院{}】有新消息：{}\n".format(parse_time(mesg_time[0]), i)
                 if not is_msg_exist(mesg):
                     flag = False
-                    msg_to_write = msg_to_write+mesg
+                    msg_to_write = msg_to_write + mesg
 
     # 福建师范大学研究生院
     req = requests.get(url_dic["fujianshifan"], headers=headers)
     req.encoding = "utf-8"
-    fushi_b = bs4.BeautifulSoup(req.text,"html.parser")
+    fushi_b = bs4.BeautifulSoup(req.text, "html.parser")
     for i in fushi_b.find_all("td"):
         mesg_time = re.findall(time_match, str(i))
         if mesg_time and parse_time(mesg_time[0]) > begin_date:
             if not i.find_all("a"):
                 continue
-            mesg = "【福建师范{}】有新消息：{}\n".format(parse_time(mesg_time[0]),re.sub(r"<.*?>|&nbsp;|\n","",str(i.find_all("a"))))
+            mesg = "【福建师范{}】有新消息：{}\n".format(parse_time(mesg_time[0]),
+                                                      re.sub(r"<.*?>|&nbsp;|\n", "", str(i.find_all("a"))))
             if not is_msg_exist(mesg):
                 flag = False
-                msg_to_write = msg_to_write+mesg
+                msg_to_write = msg_to_write + mesg
 
     # 小木虫
     for url in url_dic["xiaomuchong"]:
         req = requests.get(url, headers=headers)
-        xiaomu_b = bs4.BeautifulSoup(req.text,"html.parser")
+        xiaomu_b = bs4.BeautifulSoup(req.text, "html.parser")
         for i in xiaomu_b.find_all("tr"):
-            if not "工学"in str(i):
+            if not "工学" in str(i):
                 continue
             mesg_time = re.findall(time_match, str(i))
             if mesg_time and parse_time(mesg_time[0]) >= begin_date:
-                mesg = "【小木虫{}】{}\n".format(parse_time(mesg_time[0]),re.sub(r"<.*?>|&nbsp;|\n","",str(i)))
-                if not is_msg_exist(mesg):
+                mesg = "【小木虫{}】{}\n".format(parse_time(mesg_time[0]), re.sub(r"<.*?>|&nbsp;|\n", "", str(i)))
+                if not is_msg_exist(mesg) and is_computer_science(mesg):
                     flag = False
-                    msg_to_write = msg_to_write+mesg
+                    msg_to_write = msg_to_write + mesg
     if not flag:
         fp.write(msg_to_write)
-
 
     # TODO:研招网
 
@@ -146,9 +158,11 @@ def get_mo_id():
     mo_id = mo[0]['UserName']
     return mo_id
 
+
 def test_send():
-    msg="today"
+    msg = "today"
     wchannel.send(msg, get_mo_id())
+
 
 if __name__ == "__main__":
     sched = BlockingScheduler()
