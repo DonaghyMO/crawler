ccb30638ea7258f7c88cdc927cccb7ef70184b4a
donaghy_mo <504643635@qq.com>
2023-02-23 00:53:18 +0800

    不爬杭师了

--- crawlers/crawl_linux.py
+++ crawlers/crawl_linux.py
@@ -144,16 +144,16 @@ def crawl_msg():
                 msg_to_write = msg_to_write + mesg
 
     # 杭州师范大学
-    req = requests.get(url_dic["hangzhoushifan"], headers=headers)
-    req.encoding = "utf-8"
-    hangshifan = bs4.BeautifulSoup(req.text, "html.parser")
-    for i in hangshifan.find_all("a", attrs={"title": re.compile("^$")}):
-        mesg_time = re.findall(time_match, str(i))[0]
-        mesg = "【杭州师范大学{}】有新消息：{}\n".format(parse_time(mesg_time),
-                                                      re.sub(r"<.*?>|&nbsp;|\n", "", str(i)))
-        if not is_msg_exist(mesg,today_contain) and parse_time(mesg_time) >= begin_date:
-            flag = False
-            msg_to_write = msg_to_write + mesg
+    # req = requests.get(url_dic["hangzhoushifan"], headers=headers)
+    # req.encoding = "utf-8"
+    # hangshifan = bs4.BeautifulSoup(req.text, "html.parser")
+    # for i in hangshifan.find_all("a", attrs={"title": re.compile("^$")}):
+    #     mesg_time = re.findall(time_match, str(i))[0]
+    #     mesg = "【杭州师范大学{}】有新消息：{}\n".format(parse_time(mesg_time),
+    #                                                   re.sub(r"<.*?>|&nbsp;|\n", "", str(i)))
+    #     if not is_msg_exist(mesg,today_contain) and parse_time(mesg_time) >= begin_date:
+    #         flag = False
+    #         msg_to_write = msg_to_write + mesg
 
     # 湘潭大学
     req = requests.get(url_dic["xiangtandaxue"], headers=headers, verify=False)
