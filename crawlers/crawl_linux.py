b2d73aa93b755d60d143a14f95f367d0eaec0c0a
donaghy_mo <504643635@qq.com>
2023-02-28 19:39:28 +0800

    测试修改发送方法

--- crawlers/crawl_linux.py
+++ crawlers/crawl_linux.py
@@ -20,8 +20,8 @@ engineering = ["工学"]
 expertises = ["网络空间安全", "计算机", "电子", "网络", "计算机科学与技术", "电子信息"]
 strong_notify_schools = ["燕山大学", "广州大学", "福建师范大学", "湖北大学", "石河子大学", "杭州师范大学", "湘潭大学"]
 # 爬虫开始的日期
-begin_date = datetime.date.today()
-# begin_date = datetime.date(2022,2,15)
+# begin_date = datetime.date.today()
+begin_date = datetime.date(2022,2,15)
 
 work_directory_linux = "/root/msg/"
 work_directory_win = "D:\\msg\\"
@@ -233,7 +233,7 @@ def crawl_msg():
 def five_m_send():
     msg = crawl_msg()
     if msg:
-        wchannel.send(msg, get_mo_id())
+        itchat.send(msg, toUserName=get_mo_id())
 
 
 def after_login():
