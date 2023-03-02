c29119ed0400bf2ee0787aca45f0474134aa2875
donaghy_mo <504643635@qq.com>
2023-03-03 00:17:21 +0800

    修改过了一天重复发消息

--- crawlers/crawl_linux.py
+++ crawlers/crawl_linux.py
@@ -20,24 +20,26 @@ useless_key_words = ["无查询结果"]
 engineering = ["工学"]
 expertises = ["网络空间安全", "计算机", "电子", "网络", "计算机科学与技术", "电子信息"]
 strong_notify_schools = ["燕山大学", "广州大学", "福建师范大学", "湖北大学", "石河子大学", "杭州师范大学", "湘潭大学"]
-# 爬虫开始的日期
-begin_date = datetime.date.today()
 # begin_date = datetime.date(2022,2,15)
-
 
 # 测试用
 start_time = time.time()
 
 work_directory_linux = "/root/msg/"
 work_directory_win = "D:\\msg\\"
+def get_crawl_date():
+    return datetime.date.today()
+def get_file_name():
+    if platform.system().lower() == "linux":
+        file_name = "{}{}.txt".format(work_directory_linux,get_crawl_date())
+    else:
+        file_name = "{}{}.txt".format(work_directory_win,get_crawl_date())
+    return file_name
 
-if platform.system().lower() == "linux":
-    file_name = "{}{}.txt".format(work_directory_linux,begin_date)
-else:
-    file_name = "{}{}.txt".format(work_directory_win,begin_date)
-if not os.path.exists(file_name):
-    fp = open(file_name, "w")
-    fp.close()
+
+def get_today_contain():
+    fp_r = open(get_file_name(), "r")
+    return fp_r.read()
 
 def get_work_directory():
     if platform.system().lower() == "linux":
@@ -79,6 +81,8 @@ def is_computer_science(msg):
 
 
 def crawl_msg():
+    crawl_date = get_crawl_date()
+    file_name = get_file_name()
     url_dic = {
         "yanzhao": "https://yz.chsi.com.cn/apply/cjcx/cjcx.do",
         "fangban": "http://wyy.gzhu.edu.cn/zsjy/yjszs.htm",
@@ -97,8 +101,8 @@ def crawl_msg():
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
         "Cookie": """SF_cookie_2 = 96816998;JSESSIONID = 6B67978CB3D33B9EA650A22C65F8F2CC"""
     }
-    fp_r = open(file_name,"r")
-    today_contain = fp_r.read()
+
+    today_contain = get_today_contain()
     flag = True
     fp = open(file_name, "a")
     msg_to_write = ""
@@ -119,7 +123,7 @@ def crawl_msg():
         mesg_date_str = re.findall(time_match, str(i.find_parent()))
         if mesg_date_str:
             mesg_date = parse_time(mesg_date_str[0])
-            if mesg_date > begin_date:
+            if mesg_date > crawl_date:
                 mesg = "【广州大学方班{}】有新消息{}\n".format(mesg_date,
                                                              re.sub(r"<.*?>|&nbsp;|\n", "", str(i.find_parent())))
                 if not is_msg_exist(mesg,today_contain):
@@ -133,7 +137,7 @@ def crawl_msg():
     for i in guangda_b.find_all('a'):
         mesg_time = re.findall(time_match, str(i))
         if mesg_time:
-            if parse_time(mesg_time[0]) > begin_date:
+            if parse_time(mesg_time[0]) > crawl_date:
 
                 i = re.findall(r"title=(.+?)\"", str(i))
                 mesg = "【广州大学研究生院{}】有新消息：{}\n".format(parse_time(mesg_time[0]), i)
@@ -147,7 +151,7 @@ def crawl_msg():
     fushi_b = bs4.BeautifulSoup(req.text, "html.parser")
     for i in fushi_b.find_all("td"):
         mesg_time = re.findall(time_match, str(i))
-        if mesg_time and parse_time(mesg_time[0]) > begin_date:
+        if mesg_time and parse_time(mesg_time[0]) > crawl_date:
             if not i.find_all("a"):
                 continue
             mesg = "【福建师范{}】有新消息：{}\n".format(parse_time(mesg_time[0]),
@@ -176,8 +180,8 @@ def crawl_msg():
     for i in xiangtan_b.find_all("article", attrs={
         "class": "elementor-post elementor-grid-item post-2106 post type-post status-publish format-standard hentry category-36"}):
         msg_day = parse_xiangtan_time(delete_html(str(i.div.div)))
-        if msg_day >= begin_date:
-            msg = "【湘潭大学{}】{}\n".format(str(begin_date), delete_html(str(i)))
+        if msg_day >= crawl_date:
+            msg = "【湘潭大学{}】{}\n".format(str(crawl_date), delete_html(str(i)))
             if not is_msg_exist(msg,today_contain):
                 flag = False
                 msg_to_write = msg_to_write + msg
@@ -190,8 +194,8 @@ def crawl_msg():
         year, month, day = delete_html(str(i.parent()[0])).strip().split('-')
         year, month, day = int(year), int(month), int(day)
         crawl_day = datetime.date(year, month, day)
-        if crawl_day >= begin_date:
-            msg = "【燕山大学{}】{}\n".format(begin_date, delete_html(str(i)).strip())
+        if crawl_day >= crawl_date:
+            msg = "【燕山大学{}】{}\n".format(crawl_date, delete_html(str(i)).strip())
             if not is_msg_exist(msg, today_contain):
                 msg_to_write = msg_to_write+msg
 
@@ -202,7 +206,7 @@ def crawl_msg():
     for i in hubeidaxue_b.find_all("a", attrs={'target': "_blank"}):
         if 'title' in i.attrs.keys():
             date = parse_time(re.findall(r'\d+-\d+-\d+',str(i.parent))[0])
-            if date >= begin_date:
+            if date >= crawl_date:
                 msg = "【湖北大学{}】{}\n".format(str(date),delete_html(str(i)))
                 if not is_msg_exist(msg, today_contain):
                     msg_to_write = msg_to_write + msg
@@ -216,7 +220,7 @@ def crawl_msg():
             if not "工学" in str(i):
                 continue
             mesg_time = re.findall(time_match, str(i))
-            if mesg_time and parse_time(mesg_time[0]) >= begin_date:
+            if mesg_time and parse_time(mesg_time[0]) >= crawl_date:
                 mesg = "【小木虫{}】{}\n".format(parse_time(mesg_time[0]), re.sub(r"<.*?>|&nbsp;|\n", "", str(i)))
                 if not is_msg_exist(mesg,today_contain) and is_computer_science(mesg):
                     flag = False
@@ -242,6 +246,11 @@ def keep_alive():
 
 
 def five_m_send():
+    file_name = get_file_name()
+    if not os.path.exists(file_name):
+        fp = open(file_name, "w")
+        fp.close()
+
     msg = crawl_msg()
     if msg:
         # itchat.send(msg, toUserName=get_mo_id())
