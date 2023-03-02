8e097290eadc3e32609cc5ca3f6150ba1bbac4a8
donaghy_mo <504643635@qq.com>
2023-03-02 20:08:35 +0800

    删掉重复发

--- crawlers/crawl_linux.py
+++ crawlers/crawl_linux.py
@@ -226,7 +226,7 @@ def crawl_msg():
         fp.write(msg_to_write)
         msg_fp = open(get_work_directory()+"newmsg",'a')
         msg_fp.write(msg_to_write)
-        wchannel.send(msg_to_write,get_mo_id())
+        # wchannel.send(msg_to_write,get_mo_id())
     else:
         logger.info("正在爬取")
         # wchannel.send("持续时间：{}分钟".format((time.time()-start_time)/60),get_mo_id())
@@ -244,7 +244,7 @@ def keep_alive():
 def five_m_send():
     msg = crawl_msg()
     if msg:
-        itchat.send(msg, toUserName=get_mo_id())
+        # itchat.send(msg, toUserName=get_mo_id())
         wchannel.send(msg, get_mo_id())
 
 
